cc3e98f4d7d019b6b960d917b373d2f085cfe9c9
Casey Watson <cawatson@microsoft.com>
2022-01-25 14:51:38 -0600

    Niiiice.

--- Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
+++ Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
@@ -1,125 +1,117 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 <-n deployment_name> [-a app_service_id] [-r resource_group_name] [-s subscription_id] [-y]"; }
+THIS_MONA_VERSION="1.0"
 
-check_az() {
-    exec 3>&2
-cd
-    az version >/dev/null 2>&1
+upgrade_mona_rg() {
+    local subscription_id="$1"
+    local rg_name="$2"
+    local deployment_name="$3"
+    local web_app_name="mona-web-$deployment_name"
 
-    # TODO: Should we be more specific about which version of az is required?
+    local web_app_id=$(az resource show \
+        --resource-group "$rg_name" \
+        --subscription "$subscription_id" \
+        --name "$web_app_name" \
+        --query "id" \
+        --output "tsv")
 
-    if [[ $? -ne 0 ]]; then
-        echo "❌   Please install the Azure CLI before continuing. See [https://docs.microsoft.com/cli/azure/install-azure-cli] for more information."
+    if [[ -z "$web_app_id" ]]; then
+
+        # There should be a web app here but there isn't. Let the user know then bail early...
+
+        echo "Expected Mona web app [$web_app_name] not found in resource group [$rg_name]. Upgrade failed." >&2
+
         return 1
     else
-        echo "✔   Azure CLI installed."
+
+        # Alright so we're doing this...
+
+        local upgrade_slot_name="mona-upgrade-$(date +%s)"
+
+        # Create a temporary slot to push the new version of Mona to...
+
+        echo "Creating temporary upgrade deployment slot [$upgrade_slot_name]..."
+
+        az webapp deployment slot create \
+            --name "$web_app_name" \
+            --resource-group "$rg_name" \
+            --slot "$upgrade_slot_name" \
+            --configuration-source "$web_app_name" \
+            --subscription "$subscription_id"
+
+        
     fi
 }
 
-check_dotnet() {
-    exec 3>&2
+echo "Scanning subscriptions for upgradeable Mona deployments..."
+echo
 
-    dotnet --version >/dev/null 2>&1
+subscriptions_ids=$(az account subscription list \
+    --query "[].subscriptionId" \
+    --output "tsv")
 
-   # TODO: Should we be more specific about which version of dotnet is required?
+for subscription_id in subscription_ids; do
 
-    if [[ $? -ne 0 ]]; then
-        echo "❌   Please install .NET before continuing. See [https://dotnet.microsoft.com/download] for more information."
-        return 1
-    else
-        echo "✔   .NET installed."
-    fi
-}
+    echo "Scanning subscription [$subscription_id]..."
 
-check_prereqs() {
-    echo "Checking Mona upgrade prerequisites...";
+    # If a resource group has a "Mona Version" tag, we can be pretty confident 
+    # that it's a Mona deployment. Get all the resource groups that we can touch
+    # that probably contain a Mona deployment.
 
-    check_az;          if [[ $? -ne 0 ]]; then prereq_check_failed=1; fi;
-    check_dotnet;      if [[ $? -ne 0 ]]; then prereq_check_failed=1; fi;
+    mona_rg_names=$(az group list \
+        --subscription "$subscription_id" \
+        --query "[?tags.\"Mona Version\" != null].name" \
+        --output "tsv")
 
-    if [[ -z $prereq_check_failed ]]; then
-        echo "✔   All Mona upgrade prerequisites installed."
-    else
-        return 1
-    fi
-}
+    for mona_rg_name in mona_rg_names; do
 
-while getopts "n:a:r:s:y" opt; do
-    case $opt in
-        n)
-            p_deployment_name=$OPTARG
-        ;;
-        a)
-            p_app_service_id=$OPTARG
-        ;;
-        r)
-            p_resource_group_name=$OPTARG
-        ;;
-        s)
-            p_subscription_id=$OPTARG
-        ;;
-        y)
-            p_yes=1
-        ;;
-        \?)
-            usage
-            exit 1
-        ;;
-    esac
-done
+        # Get this resource group's Mona version.
 
-[[ -z $p_deployment_name ]] && { usage; exit 1; }
+        rg_mona_version=$(az group show \
+            --name "$mona_rg_name" \
+            --subscription "$subscription_id" \
+            --query "tags.\"Mona Version\"" \
+            --output "tsv")
 
-eg_app_service_name="mona-web-$p_deployment_name"
+        # Get this resource group's Mona deployment name.
 
-echo "Locating existing Mona app service (e.g., \"$eg_app_service_name\") for upgrade..."
+        rg_mona_name=$(az group show \
+            --name "$mona_rg_name" \
+            --subscription "$subscription_id" \
+            --query "tags.\"Deployment Name\"" \
+            --output "tsv")
 
-if [[ -z $p_app_service_id ]]; then # If a specific app service ID was _not_ provided...
-    if [[ -n $p_subscription_id ]]; then # If a specific subscription ID was provided...
-        az account set --subscription $p_subscription_id # Try to switch to it if we're not already there.
+        if [[ -z $rg_mona_name ]]; then
 
-        if [[ $? -ne 0 ]]; then
-            echo "❌   Azure subscription [$p_subscription_id] not found. Upgrade failed."
-            exit 1
+            # Well, this is awkward. Somehow, we got in a situation where we have a resource group that 
+            # presumably contains a Mona deployment but doesn't have a "Deployment Name" tag. We need to 
+            # know the name of the deployment in order to upgrade it so we'll have to skip this one. Bummer.
+
+            echo
+            echo "Azure resource group [$mona_rg_name] in subscription [$subscription_id] is tagged with Mona version [$rg_mona_version] but has no \"Deployment Name\" tag. Unable to upgrade Mona without a deployment name." >&2
+
+        elif [[ "$THIS_MONA_VERSION" -ne "$rg_mona_version" ]]; then # Different Mona version so potentially upgradeable.
+
+            # TODO: Add additional logic here to actually compare Mona versions to prevent downgrade?
+
+            echo
+            echo "Potentially upgradeable Mona deployment found."
+            echo
+            echo "Deployment Name:      [$rg_mona_name]"
+            echo "Azure Subscription:   [$subscription_id]"
+            echo "Resouce Group:        [$mona_rg_name]"
+            echo
+            echo "Current Version:      [$rg_mona_version]"
+            echo "Upgrade to Version?:  [$THIS_MONA_VERSION]"
+            echo
+
+            read -p "Upgrade Mona deployment [$rg_mona_name] to version [$THIS_MONA_VERSION]? [y/N]" confirm_upgrade
+
+            case "$confirm_upgrade" in
+                [yY1]   ) upgrade_mona_rg "$subscription_id" "$mona_rg_name" "$rg_mona_name";; # We have a winner!
+                *       ) ;;
+            esac
         fi
-    fi
-
-    if [[ -n $p_resource_group_name ]]; then # If a specific resource group name was provided...
-        resource_group_name="$p_resource_group_name" # Then use it.
-    else
-        resource_group_name="mona-$p_deployment_name" # Otherwise, assume the conventional resource group name.
-    fi
-
-    app_service_id=$(az resource show --resource-group "$resource_group_name" --name "mona-web-$p_deployment_name" --query id --output tsv)
-else
-    app_service_id=$(az resource show --ids "$p_app_service_id" --query id --output tsv)
-fi
-
-# Alright, so does the app service actually exist?
-
-if [[ -z $app_service_id ]]; then 
-    echo "❌   Existing Mona app service (e.g., \"$eg_app_service_name\") not found. Upgrade failed."
-    exit 1
-else
-    echo "Preparing to upgrade Mona web app at [$app_service_id]..."
-fi
-
-# Create the upgrade deployment slot if it doesn't already exist...
-
-upgrade_slot_name="mona-upgrade";
-
-if [[ -z $(az webapp deployment slot list --ids "$app_service_id" --query "[?name == '$upgrade_slot_name'].name" --output tsv) ]]; then
-    echo "⚠️   Mona app service upgrade slot [$upgrade_slot_name] already exists."
-
-    # TODO: Ask the user if it's cool if we blow away this deployment slot...
-
-
-fi
-
-# Create the upgrade deployment slot...
-
-echo "Creating app service upgrade slot [$upgrade_slot_name]..."
-
-
+    done
+done
 
