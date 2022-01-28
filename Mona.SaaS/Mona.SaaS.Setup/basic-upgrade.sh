dee90a072923cb89e04cf7c6400a88c26c762243
Casey Watson <cawatson@microsoft.com>
2022-01-28 15:10:57 -0600

    Continued testing...

--- Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
+++ Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
@@ -2,6 +2,45 @@
 
 THIS_MONA_VERSION="1.0"
 
+check_az() {
+    az version >/dev/null
+
+    # TODO: Should we be more specific about which version of az is required?
+
+    if [[ $? -ne 0 ]]; then
+        echo "❌   Please install the Azure CLI before continuing. See [https://docs.microsoft.com/cli/azure/install-azure-cli] for more information."
+        return 1
+    else
+        echo "✔   Azure CLI installed."
+    fi
+}
+
+check_dotnet() {
+    dotnet --version >/dev/null
+
+    # TODO: Should we be more specific about which version of dotnet is required?
+
+    if [[ $? -ne 0 ]]; then
+        echo "❌   Please install .NET before continuing. See [https://dotnet.microsoft.com/download] for more information."
+        return 1
+    else
+        echo "✔   .NET installed."
+    fi
+}
+
+check_prereqs() {
+    echo "Checking Mona upgrade prerequisites...";
+
+    check_az;       if [[ $? -ne 0 ]]; then prereq_check_failed=1; fi;
+    check_dotnet;   if [[ $? -ne 0 ]]; then prereq_check_failed=1; fi;
+
+    if [[ -z $prereq_check_failed ]]; then
+        echo "$lp ✔   All Mona upgrade prerequisites installed."
+    else
+        return 1
+    fi
+}
+
 upgrade_mona_rg() {
     local subscription_id="$1"
     local rg_name="$2"
@@ -30,18 +69,9 @@ upgrade_mona_rg() {
 
         local upgrade_slot_name="mona-upgrade-$(date +%s)"
 
-        # Alright, let's build the new version of Mona...
-
-        echo
-        echo "Packaging new Mona web app for deployment to app service [$web_app_name]..."
-        echo
-
-        dotnet publish -c Release -o ./topublish ../Mona.SaaS.Web/Mona.SaaS.Web.csproj
-        cd ./topublish
-        zip -r ../topublish.zip . >/dev/null
-        cd ..
-
         # Create a temporary slot to push the new version of Mona to...
+
+        # FYI - We used to do this _right before
 
         echo
         echo "Creating app service [$web_app_name] temporary deployment slot [$upgrade_slot_name]..."
@@ -53,6 +83,17 @@ upgrade_mona_rg() {
             --slot "$upgrade_slot_name" \
             --configuration-source "$web_app_name" \
             --subscription "$subscription_id"
+
+        # Alright, let's build the new version of Mona...
+
+        echo
+        echo "Packaging new Mona web app for deployment to app service [$web_app_name]..."
+        echo
+
+        dotnet publish -c Release -o ./topublish ../Mona.SaaS.Web/Mona.SaaS.Web.csproj
+        cd ./topublish
+        zip -r ../topublish.zip . >/dev/null
+        cd ..
 
         echo
         echo "Deploying upgraded Mona web app to app service [$web_app_name] temporary deployment slot [$upgrade_slot_name]..."
@@ -72,7 +113,6 @@ upgrade_mona_rg() {
 
         echo
         echo "Upgraded Mona web app has been deployed to app service [$web_app_name] deployment slot [$upgrade_slot_name]. Swapping production slot with upgraded deployment slot [$upgrade_slot_name]..."
-        echo
 
         az webapp deployment slot swap \
             --slot "$upgrade_slot_name" \
@@ -83,9 +123,10 @@ upgrade_mona_rg() {
             --target-slot "production"
 
         echo
-        echo "Upgraded Mona web app promoted to production slot. Please visit the upgraded Mona admin center [ https://$web_app_name.azurewebsites.net/admin ] and test landing pages [ https://$web_app_name.azurewebsites.net/test ] to confirm that your upgraded Mona deployment is working as expected."
+        echo "Upgraded Mona web app promoted to production slot."
         echo
-
+        echo "Please visit the upgraded Mona admin center [ https://$web_app_name.azurewebsites.net/admin ] and test landing pages [ https://$web_app_name.azurewebsites.net/test ] to confirm that your upgraded Mona deployment is working as expected."
+        echo
         read -p "Is Mona [$deployment_name] working as expected? [y/N] " complete_upgrade
 
         case "$complete_upgrade" in
@@ -94,9 +135,9 @@ upgrade_mona_rg() {
                 echo "Tagging Mona resource group [$rg_name] with updated Mona version..."
                 echo
                 
-                az group update \
-                    --name "$rg_name" \
-                    --subscription "$subscription_id" \
+                az tag update \
+                    --resource-id "/subscriptions/$subscription_id/resourcegroups/$rg_name" \
+                    --operation "merge" \
                     --tags "Mona Version"="$THIS_MONA_VERSION"
             ;;
             *       )
@@ -116,7 +157,6 @@ upgrade_mona_rg() {
 
         echo
         echo "Deleting app service [$web_app_name] deployment slot [$upgrade_slot_name]..."
-        echo
 
         az webapp deployment slot delete \
             --slot "$upgrade_slot_name" \
@@ -128,7 +168,17 @@ upgrade_mona_rg() {
     fi
 }
 
+cat ./spash.txt & echo;
+
+check_prereqs
+
+if [[ $? -ne 0 ]]; then
+    echo "$lp ❌   Please install all Mona setup prerequisites then try again. Setup failed."
+    exit 1
+fi
+
 echo "Scanning subscriptions for upgradeable Mona deployments..."
+echo
 
 subscription_ids=$(az account subscription list \
     --query "[].subscriptionId" \
