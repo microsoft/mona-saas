fff40ae92847f340483e19b1303904d7f54402a7
Casey Watson <cawatson@microsoft.com>
2022-01-28 20:04:17 +0000

    Local debugging...

--- Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
+++ Mona.SaaS/Mona.SaaS.Setup/basic-upgrade.sh
@@ -91,11 +91,11 @@ upgrade_mona_rg() {
 
 echo "Scanning subscriptions for upgradeable Mona deployments..."
 
-subscriptions_ids=$(az account subscription list \
+subscription_ids=$(az account subscription list \
     --query "[].subscriptionId" \
     --output "tsv")
 
-for subscription_id in subscription_ids; do
+for subscription_id in $subscription_ids; do
 
     echo "Scanning subscription [$subscription_id]..."
 
@@ -108,7 +108,7 @@ for subscription_id in subscription_ids; do
         --query "[?tags.\"Mona Version\" != null].name" \
         --output "tsv")
 
-    for mona_rg_name in mona_rg_names; do
+    for mona_rg_name in $mona_rg_names; do
 
         # Get this resource group's Mona version.
 
@@ -134,7 +134,7 @@ for subscription_id in subscription_ids; do
 
             echo "Azure resource group [$mona_rg_name (subscription: $subscription_id)] is tagged with Mona version [$rg_mona_version] but has no \"Deployment Name\" tag. Unable to upgrade Mona without a deployment name." >&2
 
-        elif [[ "$THIS_MONA_VERSION" -ne "$rg_mona_version" ]]; then # Different Mona version so potentially upgradeable.
+        elif [[ "$THIS_MONA_VERSION" != "$rg_mona_version" ]]; then # Different Mona version so potentially upgradeable.
 
             # TODO: Add additional logic here to actually compare Mona versions to prevent downgrade?
 
@@ -151,7 +151,7 @@ for subscription_id in subscription_ids; do
             read -p "Upgrade Mona deployment [$rg_mona_name] to version [$THIS_MONA_VERSION]? [y/N]" initiate_upgrade
 
             case "$initiate_upgrade" in
-                [yY1]   ) echo; echo "Too bad. This part isn't wired up yet. 🤷🏼‍♂️"; echo; # upgrade_mona_rg "$subscription_id" "$mona_rg_name" "$rg_mona_name";; # We have a winner!
+                [yY1]   ) echo; echo "Too bad. This part isn't wired up yet. 🤷🏼‍♂️"; echo; ;; # upgrade_mona_rg "$subscription_id" "$mona_rg_name" "$rg_mona_name";; # We have a winner!
                 *       ) ;; # Move along now. Nothing to see here...
             esac
         else
