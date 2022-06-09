db95434fb3921f45b10eee7b8948a35a806627ad
Casey Watson <cawatson@microsoft.com>
2022-06-09 16:34:28 -0500

    Updating license...

--- Mona.SaaS/Mona.SaaS.Core/Models/Events/V_2021_10_01/FlatSubscription.cs
+++ Mona.SaaS/Mona.SaaS.Core/Models/Events/V_2021_10_01/FlatSubscription.cs
@@ -1,14 +1,5 @@
-// MICROSOFT CONFIDENTIAL INFORMATION
-//
-// Copyright © Microsoft Corporation
-//
-// Microsoft Corporation (or based on where you live, one of its affiliates) licenses this preview code for your internal testing purposes only.
-//
-// Microsoft provides the following preview code AS IS without warranty of any kind. The preview code is not supported under any Microsoft standard support program or services.
-//
-// Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the preview code remains with you.
-//
-// In no event shall Microsoft be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the preview code, even if Microsoft has been advised of the possibility of such damages.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using Newtonsoft.Json;
 using System;
