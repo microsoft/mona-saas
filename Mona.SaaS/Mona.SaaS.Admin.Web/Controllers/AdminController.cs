edc293f7cb43d6af3682037f80dd9b3267e36147
Casey Watson <cawatson@microsoft.com>
2024-09-22 21:56:23 -0500

    Admin page is niiiice now...

--- Mona.SaaS/Mona.SaaS.Admin.Web/Controllers/AdminController.cs
+++ Mona.SaaS/Mona.SaaS.Admin.Web/Controllers/AdminController.cs
@@ -3,12 +3,11 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Mona.SaaS.Admin.Web.Models.Admin;
 using Mona.SaaS.Core.Interfaces;
 using Mona.SaaS.Core.Models.Configuration;
 using Mona.SaaS.Web.Models;
 using Mona.SaaS.Web.Models.Admin;
-using System.Text.Json;
+using System.Security.Claims;
 
 namespace Mona.SaaS.Admin.Web.Controllers
 {
@@ -37,24 +36,34 @@ namespace Mona.SaaS.Admin.Web.Controllers
         private AdminPageViewData CreateViewData(PublisherConfiguration publisherConfig) =>
             new AdminPageViewData
             {
+                DeploymentName = deploymentConfig.Name,
                 IsSetupComplete = publisherConfig.IsSetupComplete,
                 MonaVersion = deploymentConfig.MonaVersion,
                 AzureSubscriptionId = deploymentConfig.AzureSubscriptionId,
                 AzureResourceGroupName = deploymentConfig.AzureResourceGroupName,
                 EventGridTopicName = $"mona-events-{deploymentConfig.Name.ToLower()}",
+                ExternalIdentityName = $"mona-external-id-{deploymentConfig.Name.ToLower()}",
+                InternalIdentityName = $"mona-internal-id-{deploymentConfig.Name.ToLower()}",
+                AdminAppName = $"mona-admin-{deploymentConfig.Name.ToLower()}",
+                CustomerAppName = $"mona-customer-{deploymentConfig.Name.ToLower()}",
+                AdminAppSettingsUrl = GetAdminAppConfigurationSettingsEditorUrl(),
+                CustomerAppSettingsUrl = GetCustomerAppConfigurationSettingsEditorUrl(),
+                ExternalIdentityOverviewUrl = GetExternalIdentityOverviewUrl(),
+                InternalIdentityOverviewUrl = GetInternalIdentityOverviewUrl(),
                 EventGridTopicOverviewUrl = GetEventGridTopicUrl(),
                 PartnerCenterTechnicalDetails = GetPartnerCenterTechnicalDetails(),
                 ResourceGroupOverviewUrl = GetResourceGroupUrl(),
                 TestLandingPageUrl = Url.RouteUrl("landing/test", null, Request.Scheme)!,
-                TestWebhookUrl = Url.RouteUrl("webhook/test", null, Request.Scheme)!
+                TestWebhookUrl = Url.RouteUrl("webhook/test", null, Request.Scheme)!,
+                UserFriendlyName = GetUserFriendlyName()
             };
 
-        [HttpGet, Route("admin", Name = "admin")]
+        [HttpGet, Route("/", Name = "admin")]
         public async Task<IActionResult> Index()
         {
             try
             {
-                var publisherConfig = await publisherConfigStore.GetPublisherConfiguration() 
+                var publisherConfig = await publisherConfigStore.GetPublisherConfiguration()
                     ?? new PublisherConfiguration();
 
                 ViewData["admin"] = CreateViewData(publisherConfig);
@@ -69,7 +78,7 @@ namespace Mona.SaaS.Admin.Web.Controllers
             }
         }
 
-        [HttpPost, Route("admin", Name = "admin")]
+        [HttpPost, Route("/", Name = "admin")]
         public async Task<IActionResult> Index([FromForm] AdminPageModel adminModel)
         {
             try
@@ -103,10 +112,31 @@ namespace Mona.SaaS.Admin.Web.Controllers
             }
         }
 
-        private string GetConfigurationSettingsEditorUrl() =>
+        private string GetUserFriendlyName() =>
+            User?.FindFirst("given_name")?.Value ??
+            User?.FindFirst("name")?.Value ??
+            "Admin";
+
+        private string GetAdminAppConfigurationSettingsEditorUrl() =>
+            GetConfigurationSettingsEditorUrl($"mona-admin-{deploymentConfig.Name.ToLower()}");
+
+        private string GetCustomerAppConfigurationSettingsEditorUrl() =>
+            GetConfigurationSettingsEditorUrl($"mona-customer-{deploymentConfig.Name.ToLower()}");
+
+        private string GetConfigurationSettingsEditorUrl(string webAppName) =>
             $"https://portal.azure.com/#@{identityConfig.EntraTenantId}/resource/subscriptions/{deploymentConfig.AzureSubscriptionId}" +
             $"/resourceGroups/{deploymentConfig.AzureResourceGroupName}/providers/Microsoft.Web" +
-            $"/sites/mona-web-{deploymentConfig.Name.ToLower()}/configuration";
+            $"/sites/{webAppName}/environmentVariablesAppSettings";
+
+        private string GetExternalIdentityOverviewUrl() =>
+            GetIdentityOverviewUrl($"mona-external-id-{deploymentConfig.Name.ToLower()}");
+
+        private string GetInternalIdentityOverviewUrl() =>
+            GetIdentityOverviewUrl($"mona-internal-id-{deploymentConfig.Name.ToLower()}");
+
+        private string GetIdentityOverviewUrl(string identityName) =>
+            $"https://portal.azure.com/#@{identityConfig.EntraTenantId}/resource/subscriptions/{deploymentConfig.AzureSubscriptionId}" +
+            $"/resourceGroups/{deploymentConfig.AzureResourceGroupName}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/{identityName}/overview";
 
         private string GetEventGridTopicUrl() =>
             $"https://portal.azure.com/#@{identityConfig.EntraTenantId}/resource/subscriptions/{deploymentConfig.AzureSubscriptionId}" +
